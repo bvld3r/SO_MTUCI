45249e67acea9f879920151308c569e34e4c7d6d
Slava <bvldr@bk.ru>
2022-11-03 15:18:03 +0300

    v1.0

---
+++ gallery/gallery.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	
+	<title>Фотоальбом</title>
+	<link rel="stylesheet" type="text/css" href="index_style.css">
+</head>
+<body>
+	<canvas id="canvas"></canvas>			
+
+</body>
+<script  src="background_script.js" ></script>
+ 
+</html>
