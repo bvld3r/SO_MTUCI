382a3457f7456bc21c634d115946c18183af24c4
Slava <bvldr@bk.ru>
2022-11-07 00:46:28 +0300

    v1.4

--- feed/feed.php
+++ feed/feed.php
@@ -7,7 +7,15 @@
 	<link rel="stylesheet" type="text/css" href="feed_style.css">
 </head>
 <body>
-	<canvas id="canvas"></canvas>			
+	<canvas id="canvas"></canvas>
+
+	<div class="navigation">
+		<a id="logo_mtuci" href="..\"><img src="..\images\logo_mtuci_01.png""></a>
+		<a href="http://so.mtuci.ru/feed/feed.php" id="button_feed" class="navigation_button">Новости</a>
+		<a href="http://so.mtuci.ru/squads/squads.php" id="button_squads" class="navigation_button">Отряды</a>
+		<a href="http://so.mtuci.ru/gallery/gallery.php" id="button_gallery" class="navigation_button">Фотоальбом</a>
+		<a href="http://so.mtuci.ru/documents/documents.php" id="button_documents" class="navigation_button">Документы</a>
+	</div>			
 </body>
 <script  src="..\background_script.js" ></script>
  
