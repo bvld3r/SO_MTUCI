5c7bded8b5532c1e17f4ea8a43388adf45e36ff7
Gleb <asnin.gleb@bk.ru>
2022-11-08 00:37:25 +0300

    v.0.1

--- squads/squads.php
+++ squads/squads.php
@@ -1,127 +1,100 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	
 	<title>Отряды</title>
 	<link rel="stylesheet" type="text/css" href="squads_style.css">
 </head>
+
 <body>
 
-	<canvas id="canvas"></canvas>
+	<canvas id="canvas" class="background_figure"></canvas>
+
+	<header>
+		<div class="navigation">
+			<a id="logo_mtuci" href="..\"><img src="..\images\logo_mtuci_01.png"></a>
+			<a href="http://so.mtuci.ru/feed/feed.php" id="button_feed" class="navigation_button">Новости</a>
+			<a href="http://so.mtuci.ru/squads/squads.php" id="button_squads" class="navigation_button">Отряды</a>
+			<a href="http://so.mtuci.ru/gallery/gallery.php" id="button_gallery" class="navigation_button">Фотоальбом</a>
+			<a href="http://so.mtuci.ru/documents/documents.php" id="button_documents" class="navigation_button">Документы</a>
+		</div>
+	</header>
 
 
-	<div class="navigation">
-		<a id="logo_mtuci" href="..\"><img src="..\images\logo_mtuci_01.png""></a>
-		<a href="squads.php" id="button_feed" class="navigation_button">Новости</a>
-		<a href="squads.php" id="button_squads" class="navigation_button">Отряды</a>
-		<a href="squads.php" id="button_gallery" class="navigation_button">Фотоальбом</a>
-		<a href="squads.php" id="button_documents" class="navigation_button">Документы</a>
-	</div>
 
 
-	<div class="blocklogoname" id="bam">
-		<div class="container">
-    		<div class="card">
-       			<div class="face face1">
-         			<div class="content">
-           				<i class="fab fa-windows"></i> 
-           				<img class="logo" src="..\images\logo_bam_01.png">           
-           				<a href="..\squads\bam\bam.php" text-decoration:none><h2>БАМ</h2></a>
-         			</div>
-       			</div>
-       			<div class="face face2">
-        			<div class="content">
-           				<p>Студенческий отряд «БАМ» – проводники, которые путешествуют по городам. Работа в РЖД, профессиональный рост, романтика дальних дорог.</p>
-           				<a href="..\squads\bam\bam.php" type="button">Подробнее</a>
-           			</div>
-         		</div>
-       		</div>
-     	</div>
-	</div>
+	<main class="main">
+		<div class="squads_content">
 
 
-	<div class="blocklogoname" id="contact">
-		<div class="container">
-    		<div class="card">
-       			<div class="face face1">
-         			<div class="content">
-           				<i class="fab fa-windows"></i> 
-           				<img class="logo" src="..\images\logo_contact_01.png">           
-           				<a href="..\squads\bam\bam.php" text-decoration:none><h2>Контакт</h2></a>
-         			</div>
-       			</div>
-       			<div class="face face2">
-        			<div class="content">
-           				<p>Студенческий сервисный отряд «Контакт» – ребята, которые знают всё о приемной комиссии. Работа в ВУЗе, помощь абитуриентам, настоящая команда единомышленников.</p>
-           				<a href="..\squads\contact\contact.php" type="button">Подробнее</a>
-           			</div>
-         		</div>
-       		</div>
-     	</div>
-	</div>
+			<div class="container_bam">
+				<div class="content_logo">
+					<i class="fab fa-windows"></i> 
+					<img class="logo" src="..\images\logo_bam_01.png" width="300px" height="300px">           
+					<a href="..\squads\bam\bam.php" text-decoration:none><h2>БАМ</h2></a>
+				</div>
+				<div class="content_text">
+					<p>Студенческий отряд «БАМ» – проводники, которые путешествуют по городам. Работа в РЖД, профессиональный рост, романтика дальних дорог.</p>
+					<a href="..\squads\bam\bam.php" type="button">Подробнее</a>
+				</div>
+			</div>
 
 
-	<div class="blocklogoname" id="svyaz">
-		<div class="container">
-    		<div class="card">
-       			<div class="face face1">
-         			<div class="content">
-           				<i class="fab fa-windows"></i> 
-           				<img class="logo" src="..\images\logo_svyaz_01.png">           
-           				<a href="..\squads\svyaz\svyaz.php" text-decoration:none><h2>Связь</h2></a>
-         			</div>
-       			</div>
-       			<div class="face face2">
-        			<div class="content">
-           				<p>Студенческий отряд «Связь» – вожатые, работающие в лагерях по всей России, получающие знания и опыт, который ты пронесешь через всю жизнь.</p>
-           				<a href="..\squads\svyaz\svyaz.php" type="button">Подробнее</a>
-           			</div>
-         		</div>
-       		</div>
-     	</div>
-	</div>
-
-	<div class="blocklogoname" id="way">
-		<div class="container">
-    		<div class="card">
-       			<div class="face face1">
-         			<div class="content">
-           				<i class="fab fa-windows"></i> 
-           				<img class="logo" src="..\images\logo_way_01.png">           
-           				<a href="..\squads\way\way.php" text-decoration:none><h2>Путь</h2></a>
-         			</div>
-       			</div>
-       			<div class="face face2">
-        			<div class="content">
-           				<p>Отряд Снежного десанта «Путь» – добровольцы, делающие мир добрее и лучше. Концерты, профориентационная работа и шефская помощь населению в удаленных уголках России.</p>
-           				<a href="..\squads\way\way.php" type="button">Подробнее</a>
-           			</div>
-         		</div>
-       		</div>
-     	</div>
-	</div>
+			<div class="container_contact">
+				<div class="content_logo">
+					<i class="fab fa-windows"></i> 
+					<img class="logo" src="..\images\logo_contact_01.png" width="300px" height="300px">           
+					<a href="..\squads\bam\bam.php" text-decoration:none><h2>Контакт</h2></a>
+				</div>
+				<div class="content_text">
+					<p>Студенческий сервисный отряд «Контакт» – ребята, которые знают всё о приемной комиссии. Работа в ВУЗе, помощь абитуриентам, настоящая команда единомышленников.</p>
+					<a href="..\squads\contact\contact.php" type="button">Подробнее</a>
+				</div>
+			</div>
 
 
-	<div class="blocklogoname" id="it">
-		<div class="container">
-    		<div class="card">
-       			<div class="face face1">
-         			<div class="content">
-           				<i class="fab fa-windows"></i> 
-           				<img class="logo" src="..\images\logo_it_01.png">           
-           				<a href="..\squads\it\it.php" text-decoration:none><h2>IT отряд</h2></a>
-         			</div>
-       			</div>
-       			<div class="face face2">
-        			<div class="content">
-           				<p>IT-отряд – студенты, проходящие бесплатное обучение IT-профессиям: web-разработчик, маркетолог, сис. аналитик, менеджер IT-проектов и UI/UX дизайнер.</p>
-           				<a href="..\squads\it\it.php" type="button">Подробнее</a>
-           			</div>
-         		</div>
-       		</div>
-     	</div>
-	</div>
+			<div class="container_communication">
+				<div class="content_logo">
+					<i class="fab fa-windows"></i> 
+					<img class="logo" src="..\images\logo_svyaz_01.png" width="300px" height="300px">           
+					<a href="..\squads\svyaz\svyaz.php" text-decoration:none><h2>Связь</h2></a>
+				</div>
+				<div class="content_text">
+					<p>Студенческий отряд «Связь» – вожатые, работающие в лагерях по всей России, получающие знания и опыт, который ты пронесешь через всю жизнь.</p>
+					<a href="..\squads\svyaz\svyaz.php" type="button">Подробнее</a>
+				</div>
+			</div>
+
+			<div class="container_way">
+				<div class="content_logo">
+					<i class="fab fa-windows"></i> 
+					<img class="logo" src="..\images\logo_way_01.png" width="300px" height="300px">           
+					<a href="..\squads\way\way.php" text-decoration:none><h2>Путь</h2></a>
+				</div>
+				<div class="content_text">
+					<p>Отряд Снежного десанта «Путь» – добровольцы, делающие мир добрее и лучше. Концерты, профориентационная работа и шефская помощь населению в удаленных уголках России.</p>
+					<a href="..\squads\way\way.php" type="button">Подробнее</a>
+				</div>
+			</div>
+
+
+			<div class="container_it">
+				<div class="content_logo">
+					<i class="fab fa-windows"></i> 
+					<img class="logo" src="..\images\logo_it_01.png" width="300px" height="300px">           
+					<a href="..\squads\it\it.php" text-decoration:none><h2>IT отряд</h2></a>
+				</div>
+				<div class="content_text">
+					<p>IT-отряд – студенты, проходящие бесплатное обучение IT-профессиям: web-разработчик, маркетолог, сис. аналитик, менеджер IT-проектов и UI/UX дизайнер.</p>
+					<a href="..\squads\it\it.php" type="button">Подробнее</a>
+				</div>
+			</div>
+
+		</div>
+	</main>
+	
 
 
 
