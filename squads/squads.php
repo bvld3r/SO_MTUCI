9b381b5c44cdc98b338471644eeaae803c96b941
Gleb <asnin.gleb@bk.ru>
2022-11-09 13:31:13 +0300

    v.0.2

--- squads/squads.php
+++ squads/squads.php
@@ -14,11 +14,14 @@
 
 	<header>
 		<div class="navigation">
-			<a id="logo_mtuci" href="..\"><img src="..\images\logo_mtuci_01.png"></a>
-			<a href="http://so.mtuci.ru/feed/feed.php" id="button_feed" class="navigation_button">Новости</a>
-			<a href="http://so.mtuci.ru/squads/squads.php" id="button_squads" class="navigation_button">Отряды</a>
-			<a href="http://so.mtuci.ru/gallery/gallery.php" id="button_gallery" class="navigation_button">Фотоальбом</a>
-			<a href="http://so.mtuci.ru/documents/documents.php" id="button_documents" class="navigation_button">Документы</a>
+			<a id="logo_mtuci" href="..\"><img src="..\images\logo_mtuci_01.png" height="70px" width="140px"></a>
+			<div class="navigation_buttons">
+				<a href="http://so.mtuci.ru/feed/feed.php" id="button_feed" class="navigation_button">Новости</a>
+				<a href="http://so.mtuci.ru/squads/squads.php" id="button_squads" class="navigation_button">Отряды</a>
+				<a href="http://so.mtuci.ru/gallery/gallery.php" id="button_gallery" class="navigation_button">Фотоальбом</a>
+				<a href="http://so.mtuci.ru/documents/documents.php" id="button_documents" class="navigation_button">Документы</a>
+			</div>
+
 		</div>
 	</header>
 
